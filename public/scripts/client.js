f9cf86e5675a05ec7e233216bb30384fbebed7e6
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-20 05:03:39 +0000

    Fix linting errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,82 +1,77 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function() {
-  
+
   const renderTweets = function(tweets) {
-   let $tweets = {}; 
-    for(let i of tweets.reverse()){
-     $tweets =  createTweetElement(i)
+    let $tweets = {};
+    for (let i of tweets.reverse()) {
+      $tweets = createTweetElement(i);
       $('#tweets-container').append($tweets);
     }
-    
-  }
-  const escape = function (str) {
+
+  };
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-const createTweetElement = (Data) => {
-  const $tweet = $( `<article class = "tweet"> 
+  const createTweetElement = (Data) => {
+    const $tweet = $(`<article class = "tweet"> 
   <header>
-    <label> <img src="${Data.user.avatars}"> ${Data.user.name} </label>
-    <label> ${Data.user.handle} </label>
+    <label> <img src="${escape(Data.user.avatars)}"> ${escape(Data.user.name)} </label>
+    <label> ${escape(Data.user.handle)} </label>
   </header>
   <footer>
-   <label id = "text" for="tweet"> ${Data.content.text}</label>
+   <label id = "text" for="tweet"> ${escape(Data.content.text)}</label>
    <hr>
    <div>
-   <p id = "time">${timeago.format(Data.created_at)}</p>
+   <p id = "time">${escape(timeago.format(Data.created_at))}</p>
    <p><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p>
   </div>
   </footer>
   </article>`);
-  return $tweet;
-}
-const loadTweets = () => {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (data) {
-    renderTweets(data);
-  });
-}
+    return $tweet;
+  };
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(data) {
+        renderTweets(data);
+      });
+  };
   const loadNewTweets = () => {
     let len = 0;
     $.ajax('/tweets', { method: 'GET' })
-    .then(function (data) {
-      len = data.length - 1;
-     let ntweet = createTweetElement(data[len]);
-      $('#tweets-container').prepend(ntweet);
-    });
-  
-}
-$("form").on("submit", function (event) {
-  event.preventDefault();
-  const $input = document.getElementById("tweet-text");
-  const $counter = document.getElementById("counter");
-  if($( this ).serialize().length === 5){
-    $("#error").html("⚠️ Your tweet can't be empty tell us whats on your mind! ⚠️");
-    $("#error").hide();
-   return $("#error").slideDown("slow");
-  }
-  if($( this ).serialize().length > 145){
-    $("#error").html("⚠️ Sorry your tweet is above our limit ⚠️");
-    $("#error").hide();
-    return $("#error").slideDown("slow");
-  }
-  $("#error").slideUp("slow");
-  $.post( "/tweets", $( this ).serialize() )
-  .then(function(){
-    loadNewTweets();
-  })
-  $input.value = "";
-  $counter.innerHTML = 140;
+      .then(function(data) {
+        len = data.length - 1;
+        let ntweet = createTweetElement(data[len]);
+        $('#tweets-container').prepend(ntweet);
+      });
+
+  };
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    const $input = document.getElementById("tweet-text");
+    const $counter = document.getElementById("counter");
+    if ($(this).serialize().length === 5) {
+      $("#error").html("⚠️ Your tweet can't be empty tell us whats on your mind! ⚠️");
+      $("#error").hide();
+      return $("#error").slideDown("slow");
+    }
+    if ($(this).serialize().length > 145) {
+      $("#error").html("⚠️ Sorry your tweet is above our limit ⚠️");
+      $("#error").hide();
+      return $("#error").slideDown("slow");
+    }
+    $("#error").slideUp("slow");
+    $.post("/tweets", $(this).serialize())
+      .then(function() {
+        loadNewTweets();
+      });
+    $input.value = "";
+    $counter.innerHTML = 140;
   });
 
-  $("#newTweet").on("click", function (event) {
+  $("#newTweet").on("click", function() {
     const $input = document.getElementById("tweet-text");
     $input.focus();
-    });
+  });
   loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
   const $input = $("textarea");
   const $counter = document.getElementById("counter");
-  let index = 140
+  let index = 140;
 
   $input.on('keydown', function(event) {
     let key = event.key;
     
-    if($(this).serialize().length === 5){
+    if ($(this).serialize().length === 5) {
       index = 140;
       $counter.style.color = "black";
     }
-    if(index <= 0  && key !== "Backspace"){
+    if (index <= 0  && key !== "Backspace") {
       index--;
       $counter.style.color = "red";
       $counter.innerHTML = index;
     }
-    if(key === "Backspace" && $(this).serialize().length !== 5){
+    if (key === "Backspace" && $(this).serialize().length !== 5) {
       index++;
       $counter.style.color = "black";
       $counter.innerHTML = index;
-    } 
-    if(index > 0 && key !== "Backspace"){
+    }
+    if (index > 0 && key !== "Backspace") {
       index--;
-    $counter.innerHTML = index;
+      $counter.innerHTML = index;
     }
   });
 });
